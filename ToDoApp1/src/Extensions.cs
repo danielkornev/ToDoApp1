9d3408c38b3b71d46c40b9d999a73605708fc45a
daniel@zetuniverse.com <daniel@zetuniverse.com>
2016-06-29 15:37:53 +0200

    added Map extension method

--- ToDoApp1/src/Extensions.cs
+++ ToDoApp1/src/Extensions.cs
@@ -6,11 +6,13 @@ using System.Threading.Tasks;
 
 namespace ToDoApp1
 {
-	static class StringExtensions
+	static class Extensions
 	{
 		public static string[] Concat(this string[] source, string element)
 		{
 			return source.Concat(new[] { element }).ToArray();
 		}
+
+		public static IEnumerable<TResult> Map<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, int, TResult> selector) => source.Select(selector);
 	}
 }
