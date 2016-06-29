52b9f99dfa8e3ce4e14dd5d0f4fff4a7ec586504
daniel@zetuniverse.com <daniel@zetuniverse.com>
2016-06-29 14:49:23 +0200

    first simple running WPF app using NReact.

--- ToDoApp1/App.xaml.cs
+++ ToDoApp1/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using NReact;
 using System.Windows;
 
 namespace ToDoApp1
@@ -13,5 +8,10 @@ namespace ToDoApp1
 	/// </summary>
 	public partial class App : Application
 	{
-	}
-}
+		void Application_Startup(object sender, StartupEventArgs e)
+		{
+			MainWindow.Render(new TodoApp());
+			MainWindow.Show();
+		}
+	} // class
+} // namespace
---
+++ ToDoApp1/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp1
+{
+	static class StringExtensions
+	{
+		public static string[] Concat(this string[] source, string element)
+		{
+			return source.Concat(new[] { element }).ToArray();
+		}
+	}
+}
