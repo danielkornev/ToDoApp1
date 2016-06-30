df3ec22c7f312ab4eb1eca614e1b6cfcb51303ae
daniel@zetuniverse.com <daniel@zetuniverse.com>
2016-06-30 22:05:17 +0200

    Added test RendersAnElement
    See http://www.theodo.fr/blog/2016/03/getting-started-with-react-redux-and-immutable-a-test-driven-tutorial-part-1/ for details ("renders an item")

---
+++ TodoApp1Tests/src/components/TodoItemTests.cs
@@ -0,0 +1,29 @@
+﻿using NReact;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using ToDoApp1;
+
+namespace TodoApp1Tests
+{
+	[TestFixture, Apartment(ApartmentState.STA)]
+	public class TodoItemTests
+	{
+		[Test, Description("renders an item")]
+		public void RendersAnItem()
+		{
+			var todoitem = new TodoItem(new TodoListItem(0, "React", TodoListItem.Statuses.Active));
+
+			var component = todoitem.Render();
+
+			var tb = component.RenderAsFrameworkElement() as TextBlock;
+
+			Assert.AreEqual("React", tb?.Text);
+		}
+	} // class
+} // namespace
