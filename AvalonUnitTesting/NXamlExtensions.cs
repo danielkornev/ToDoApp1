32fe0a5c103517ed9342ae74999bef0867838290
daniel@zetuniverse.com <daniel@zetuniverse.com>
2016-06-30 14:30:33 +0200

    added new NReact RenderAsFrameworkElement extension
    It helps in unit testing by rendering NXaml component into a WPF object.

---
+++ AvalonUnitTesting/NXamlExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace NReact
+{
+	public static class NXamlExtensions
+	{
+		public static FrameworkElement RenderAsFrameworkElement(this NElement nelement)
+		{
+			var cc = new ContentControl();
+			cc.Render(nelement);
+
+			return cc.Content as FrameworkElement;
+		}
+	} // class
+} // namespace
