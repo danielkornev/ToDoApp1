0ea71864c1fd66d2eddee92e25afaba56e92a036
daniel@zetuniverse.com <daniel@zetuniverse.com>
2016-06-30 12:48:24 +0200

    added WPFUnitTesting (nee AvalonUnitTesting) from https://github.com/jogibear9988/avaloncontrolslib/blob/master/AvalonUnitTesting/AvalonTraceListener.cs
    see https://marlongrech.wordpress.com/2007/10/14/wpf-unit-testing/

---
+++ AvalonUnitTesting/WPFDataBindingTraceTester.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace WPFUnitTesting
+{
+	internal class WPFDataBindingTraceTester
+	{
+		private static WPFTraceListener dataBindingListener;
+
+		/// <summary>
+		/// Gets a databinding trace listener
+		/// </summary>
+		internal static WPFTraceListener DataBindingListener
+		{
+			get
+			{
+				if (dataBindingListener == null)
+					dataBindingListener = new WPFTraceListener();
+				return dataBindingListener;
+			}
+		}
+
+		/// <summary>
+		/// hooks the window to the wpf trace
+		/// </summary>
+		/// <param name="windowToTest">The window to test</param>
+		public static void TestDataBindingsForObject(Window windowToTest)
+		{
+			EnforceDataBindingTraceListener(windowToTest);
+			windowToTest.ShowInTaskbar = false;
+			windowToTest.Show();
+			windowToTest.Hide();
+		}
+
+		//check if there is already a databinding trace listener, if not creates it
+		private static void EnforceDataBindingTraceListener(Window currentWindow)
+		{
+			PresentationTraceSources.Refresh();
+			PresentationTraceSources.DataBindingSource.Switch.Level = SourceLevels.Warning;
+
+			//add the databinding listener
+			if (!PresentationTraceSources.DataBindingSource.Listeners.Contains(DataBindingListener))
+				PresentationTraceSources.DataBindingSource.Listeners.Add(DataBindingListener);
+
+			//set the current window being tested
+			DataBindingListener.CurrentWindowBeingTested = currentWindow;
+		}
+	} // class
+} // namespace
---
+++ AvalonUnitTesting/WPFTestRunner.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFUnitTesting
+{
+	/// <summary>
+	/// base class for unit test of wpf
+	/// for more info visit
+	/// http://marlongrech.wordpress.com/2007/10/14/wpf-unit-testing/
+	/// </summary>
+	public class WPFTestRunner
+	{
+
+		/// <summary>
+		/// Runs a set of tests for data binding
+		/// </summary>
+		/// <param name="objectToTest"></param>
+		public static void RunDataBindingTests(object objectToTest)
+		{
+			//get the window instance. This will create a wrapper window if a window is not passes as parameter
+			Window windowToTest = WPFActivatorHelper.VerifyObjectType(objectToTest);
+			if (windowToTest != null)
+				WPFDataBindingTraceTester.TestDataBindingsForObject(windowToTest);
+
+		}
+
+		private static readonly STAOperationRunner runner = new STAOperationRunner();
+
+		/// <summary>
+		/// Runs a delegate in a STA thread
+		/// </summary>
+		/// <param name="userDelegate">The operation to run</param>
+		public static void RunInSTA(ThreadStart userDelegate)
+		{
+			runner.RunInSTA(userDelegate);
+		}
+	} // class
+} // namespace
