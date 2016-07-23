3a1b4c91d205faba9cf8e205f60d0a2b83fc8d35
Daniel Kornev <daniel@zetuniverse.com>
2016-07-23 13:01:18 +0300

    added TodoHeader and test for it
    But not yet integrated it into the main TodoApp

---
+++ ToDoApp1/src/components/TodoHeader.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using NReact;
+
+namespace ToDoApp1
+{
+    public class TodoHeader : NClass
+    {
+        private Action<string> addItem;
+
+        public TodoHeader(Action<string> addItem)
+        {
+            this.addItem = addItem;
+        }
+
+        public override NElement Render()
+        {
+            return
+                new NXaml<StackPanel>().
+                    Key("todoHeader").
+                    HorizontalAlignment(HorizontalAlignment.Stretch).
+                    Orientation(Orientation.Vertical).
+                    Children(
+
+                        new NXaml<TextBlock>().
+                            Text("TODOS").
+                            Style("todoHeaderH1").
+                            HorizontalAlignment(HorizontalAlignment.Center).
+                            Margin(0, 0, 0, 50),
+
+                        new NXaml<TextBlock>().
+                            Text("What needs to be done?").
+                            HorizontalAlignment(HorizontalAlignment.Center).
+                            Margin(0, 0, 0, 10),
+
+                        new NXaml<TextBox>().
+                            Key("addTodoInput").
+                            Text("").
+                            HorizontalAlignment(HorizontalAlignment.Stretch).
+                            Margin(0, 0, 0, 10).
+                            KeyDown(this._OnKeyDown)
+                    );
+        }
+
+        private void _OnKeyDown(object sender, EventArgs e)
+        {
+            var args = e as KeyEventArgs;
+            var addTodoInput = sender as TextBox;
+
+            if (args?.Key == System.Windows.Input.Key.Enter 
+                && 
+                addTodoInput!=null
+                &&
+                addTodoInput.Text.Length>0)
+            {
+                this.addItem(addTodoInput.Text);
+            }
+        }
+    }
+}
---
+++ TodoApp1Tests/src/components/TodoHeaderTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System.Windows.Input;
+using Moq;
+using NReact;
+
+namespace ToDoApp1.Tests
+{
+    [TestFixture, Apartment(ApartmentState.STA)]
+    public class TodoHeaderTests : MockAppTests
+    {
+        [Test, Description("calls a callback on submit")]
+        public void CallsCallbackOnSubmit()
+        {
+            var addedItem = "";
+
+            Action<string> addItem = (item) =>
+            {
+                addedItem = item;
+            };
+
+            var component = new TodoHeader(addItem).Render().RenderAsFrameworkElement() as StackPanel;
+
+            var textBox = component.Children.OfType<TextBox>().First();
+
+            textBox.Text = "This is a new item";
+
+            textBox?.RaiseEvent(new KeyEventArgs(Keyboard.PrimaryDevice, new Mock<PresentationSource>().Object, 0, Key.Enter) { RoutedEvent = TextBox.KeyDownEvent });
+
+            Assert.AreEqual(addedItem, "This is a new item");
+        }
+    }
+}
