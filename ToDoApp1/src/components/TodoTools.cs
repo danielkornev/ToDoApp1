be9aa30fa0aa82eb0eb93f1f296328f5fd520fa4
Daniel Kornev <daniel@zetuniverse.com>
2016-07-25 17:51:34 +0300

    look-n-feel is even more similar to the original todomvc app
    see also https://www.facebook.com/photo.php?fbid=10154445019758783&set=a.392490598782.196903.702983782&type=3&theater
    
    includes Colors.pptx with color analysis of the original todomvc app

--- ToDoApp1/src/components/TodoTools.cs
+++ ToDoApp1/src/components/TodoTools.cs
@@ -1,5 +1,9 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+using System.Windows.Shapes;
 using NReact;
 
 namespace ToDoApp1
@@ -7,11 +11,14 @@ namespace ToDoApp1
     public class TodoTools : NClass
     {
         private string activeItems;
+        private int nbActiveItems;
 
         public TodoTools(int nbActiveItems)
         {
+            this.nbActiveItems = nbActiveItems;
+
             if (nbActiveItems == 0)
-                activeItems = string.Empty;
+                activeItems = "0 items left";
             else if (nbActiveItems == 1)
                 activeItems = "1 item left";
             else
@@ -22,15 +29,92 @@ namespace ToDoApp1
         {
             return
                 new NXaml<StackPanel>().
-                    HorizontalAlignment(HorizontalAlignment.Center).
-                    Orientation(Orientation.Horizontal).
                     Children(
 
-                        new NXaml<TextBlock>().
-                            Text(this.activeItems).
-                            Set(new NProperties().Name, "itemsNumber").
-                            Style("itemsLeft")
+                    new NXaml<Border>().
+                        Set(new NProperties().SnapsToDevicePixels, true).
+                        Set(new NProperties().DataContext, nbActiveItems).
+                        BorderBrush("#FFC7C7C7").
+                        BorderThickness(1, 0, 1, 1).
+                        Child(
+                        
+                        #region Grid
+                        new NXaml<Grid>().
+                            Style("todoTools").     
+                            ColumnDefinitions(
+                                new NXaml<ColumnDefinition>().
+                                    Width("1*"),
 
+                                new NXaml<ColumnDefinition>().
+                                    Width("1*"),
+
+                                new NXaml<ColumnDefinition>().
+                                    Width("1*")
+                            ).
+                            Children(
+
+            #region Todo Tools
+                                new NXaml<TextBlock>().
+                                    GridColumn(0).
+                                    Text(this.activeItems).
+                                    Set(new NProperties().Name, "itemsNumber").
+                                    Style("itemsLeft"),
+
+                                new NXaml<StackPanel>().
+                                    GridColumn(1).
+                                    Style("filtersPanel").
+                                    Orientation(Orientation.Horizontal).
+                                    Children(
+
+                                        new NXaml<RadioButton>().
+                                            Style("radioAsToggleButtonStyle").
+                                            Content("All").
+                                            Set(new NProperties().GroupName, "Filters"),
+
+                                        new NXaml<RadioButton>().
+                                            Style("radioAsToggleButtonStyle").
+                                            Content("Active").
+                                            Set(new NProperties().GroupName, "Filters"),
+
+                                        new NXaml<RadioButton>().
+                                            Style("radioAsToggleButtonStyle").
+                                            Content("Completed").
+                                            Set(new NProperties().GroupName, "Filters")
+                                    ),
+
+                                new NXaml<TextBlock>().
+                                    GridColumn(2).
+                                    Text("Clear completed").
+                                    Style("clearCompleted")
+            #endregion
+                                    )
+
+                        #endregion
+
+                        ),
+
+                        //new NXaml<Border>().
+                        //    BorderBrush("#FF4d4d4d").
+                        //    BorderThickness(0, 1, 0, 0).
+                        //    Set(new NProperties().SnapsToDevicePixels, true).
+                        //    Width(550).
+                        //    Height(1),
+
+                    new NXaml<Border>().
+                    Background("#FFF6F6F6").
+                    Set(new NProperties().SnapsToDevicePixels, true).
+                    Width(540).
+                    Height(5).
+                    BorderThickness(1, 0, 1, 1).
+                    BorderBrush("#FFC7C7C7"),
+
+                    new NXaml<Border>().
+                    Background("#FFF6F6F6").
+                    Set(new NProperties().SnapsToDevicePixels, true).
+                    Width(530).
+                    Height(5).
+                    BorderThickness(1, 0, 1, 1).
+                    BorderBrush("#FFC7C7C7")
                     );
         }
     }
